1377231a84e401dcac571ef6ce6288b33e092fe3
Arielle Batasin <dev.abatasin@gmail.com>
2022-06-09 22:23:08 +0800

    - updated login styles file name

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from '../styles/Forms.module.css';
+import styles from '../styles/Login.module.css';
 
 const LogIn = () => {
   return (
