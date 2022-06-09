ec96fc1850413122f9553b13826a2d73d4d93311
mary11lane <lanebasa22@gmail.com>
2022-06-09 22:08:03 +0800

    registration page

--- client/src/pages/PasswordVerification.jsx
+++ client/src/pages/PasswordVerification.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styles from '../styles/Forms.module.css';
+// import styles from '../styles/Forms.module.css';
 
 const PasswordVerification = () => {
   return <div>Password Verification</div>;
