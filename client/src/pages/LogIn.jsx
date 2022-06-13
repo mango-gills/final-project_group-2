d554f41e79c2bc321b92067831d50bd4ef35c08a
Remcel Salmasan <salmasanremcel@gmail.com>
2022-06-13 15:32:46 +0800

    login register using email and passowrd working

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../styles/Login.module.css';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth, db } from '../firebase';
+import { updateDoc, doc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
 
 const LogIn = () => {
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+    error: null,
+    loading: false,
+  });
+
+  const history = useNavigate();
+
+  const { email, password, error, loading } = data;
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setData({ ...data, error: null, loading: true });
+    if (!email || !password) {
+      return setData({ ...data, error: 'All fields are required' });
+    }
+    try {
+      const result = await signInWithEmailAndPassword(auth, email, password);
+
+      await updateDoc(doc(db, 'users', result.user.uid), {
+        isOnline: true,
+      });
+      setData({
+        email: '',
+        password: '',
+        error: null,
+        loading: false,
+      });
+      history('/');
+    } catch (err) {
+      setData({ ...data, error: err.message, loading: false });
+    }
+  };
   return (
     <div className={styles.section}>
       <div className={styles.section__container}>
@@ -10,20 +52,27 @@ const LogIn = () => {
         <p className={`${styles.container__subheading}`}>
           Please sign in to continue.
         </p>
-        <form className={styles.container__form}>
+        <form className={styles.container__form} onSubmit={handleSubmit}>
           <input
             className={styles.form__input__email}
-            type="email"
+            type="text"
+            name="email"
             placeholder="Email"
+            value={email}
+            onChange={handleChange}
           />
           <input
             className={styles.form__input__password}
             type="password"
+            name="password"
             placeholder="Password"
+            value={password}
+            onChange={handleChange}
           />
           <button className={styles.form__button} type="submit">
             Login
           </button>
+          {error ? <p className="error">{error}</p> : null}
         </form>
         <p className={styles.container__text}>
           Don't have an account yet?{' '}
--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -1,32 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdPresentToAll } from 'react-icons/md';
-
 import styles from '../styles/Registration.module.css';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, db } from '../firebase';
+import { setDoc, doc, Timestamp } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
 
 const Registration = () => {
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    error: null,
+    loading: false,
+  });
+
+  const history = useNavigate();
+
+  const { name, email, password, error, loading } = data;
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setData({ ...data, error: null, loading: true });
+    if (!name || !email || !password) {
+      return setData({ ...data, error: 'All fields are required' });
+    }
+    try {
+      const result = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      await setDoc(doc(db, 'users', result.user.uid), {
+        uid: result.user.uid,
+        name,
+        email,
+        createdAt: Timestamp.fromDate(new Date()),
+        isOnline: true,
+      });
+      setData({
+        name: '',
+        email: '',
+        password: '',
+        error: null,
+        loading: false,
+      });
+      history('/');
+    } catch (err) {
+      setData({ ...data, error: err.message, loading: false });
+    }
+  };
   return (
     <main className={styles.maincontainer}>
       <div className={styles.title}>Create an account</div>
       <section>
-        <form className={styles.registration}>
+        <form className={styles.registration} onSubmit={handleSubmit}>
           {/* <MdPresentToAll className={styles.icon} /> */}
           <input
             className={`${styles.username} ${styles.icon}`}
             id="username"
+            name="name"
             placeholder="username"
-            type="text"
+            value={name}
+            onChange={handleChange}
           ></input>
           <input
             className={styles.email}
             id="email"
             placeholder="email"
             type="text"
+            name="email"
+            value={email}
+            onChange={handleChange}
           ></input>
           <input
             className={styles.password}
             id="password"
             placeholder="password"
             type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
           ></input>
           <input
             className={styles.password}
@@ -34,7 +92,9 @@ const Registration = () => {
             placeholder="confirm password"
             type="password"
           ></input>
+
           <button className={styles.submit}>SIGNUP</button>
+          {error ? <p className="error">{error}</p> : null}
           <div className={styles.text}></div>
           <div className={styles.text}></div>
         </form>
