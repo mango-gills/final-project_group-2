b1132abe4e5cdfdc80deec3fbf60014a57d02a1f
Mark Joseph Manongsong <markmanongsong05@gmail.com>
2022-06-16 22:40:20 +0800

    Added Password_Recovery fuction page

--- client/src/pages/PasswordRecovery.jsx
+++ client/src/pages/PasswordRecovery.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import styles from '../styles/Password.module.css';
+import { auth } from '../firebase';
+import { duration } from 'moment';
 
 const PasswordRecovery = () => {
+  const [email, setEmail] = useState('');
+
+  const forgotPassword = (email) => {
+    return sendPasswordResetEmail(auth, email, {
+      url: 'http://localhost:3000/login',
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    forgotPassword(email)
+      .then((response) => {
+        console.log(response);
+        toast('Email sent, check your email', {
+          theme: 'dark',
+        });
+      })
+      .catch((e) => console.log(e.message));
+  };
+
   return (
     <main className={styles.main__container}>
       <section className={styles.main__section}>
         <div className={styles.heading}>Recover Password</div>
-        <form className={styles.form__password}>
+        <form className={styles.form__password} onSubmit={handleSubmit}>
           <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className={`${styles.form__input} ${styles.email}`}
             id="email"
             placeholder="email address"
@@ -17,6 +45,7 @@ const PasswordRecovery = () => {
           ></input>
           <button className={styles.form__button}>SUBMIT</button>
         </form>
+        <ToastContainer />
         <Link className={styles.backlink} to="/login">
           Go back to Login page
         </Link>
--- client/src/pages/PasswordReset.jsx
+++ client/src/pages/PasswordReset.jsx
@@ -1,15 +1,57 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { confirmPasswordReset } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { auth } from '../firebase';
 
 import styles from '../styles/Password.module.css';
+import { useState } from 'react';
 
 const PasswordReset = () => {
+  const resetPassword = (oobCode, newPassword) => {
+    return confirmPasswordReset(auth, oobCode, newPassword);
+  };
+
+  const useQuery = () => {
+    const location = useLocation();
+    return new URLSearchParams(location.search);
+  };
+
+  const navigate = useNavigate();
+
+  const query = useQuery();
+  // console.log(query.get('mode'));
+  // console.log(query.get('oobCode'));
+  // console.log(query.get('continueUrl'));
+
+  const [newPassword, setNewPassword] = useState('');
+
+  const handleChange = async (e) => {
+    e.preventDefault();
+
+    await resetPassword(query.get('oobCode'), newPassword)
+      .then(
+        toast('Password Has been changed', {
+          theme: 'dark',
+          autoClose: 3000,
+        })
+      )
+      .catch((err) => console.log(err.message));
+    setTimeout(() => {
+      navigate('/login');
+    }, 3000);
+  };
+
   return (
     <main className={styles.main__container}>
       <section className={styles.main__section}>
         <div className={styles.heading}>Reset Password</div>
-        <form className={styles.form__password}>
+        <form className={styles.form__password} onSubmit={handleChange}>
           <input
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
             className={`${styles.form__input} ${styles.password}`}
             id="password"
             placeholder="password"
@@ -22,7 +64,9 @@ const PasswordReset = () => {
             type="password"
           ></input>
           <button className={styles.form__button}>SUBMIT</button>
+          <ToastContainer />
         </form>
+
         <Link className={styles.backlink} to="/login">
           Go back to Login page
         </Link>
