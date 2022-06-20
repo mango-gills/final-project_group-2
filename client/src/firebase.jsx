c8f920c1232c25e3061b2acc1c9eb7941e529617
mary11lane <lanebasa22@gmail.com>
2022-06-20 20:26:56 +0800

    replaced firebase config

--- client/src/firebase.jsx
+++ client/src/firebase.jsx
@@ -3,14 +3,25 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
+// const firebaseConfig = {
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyCL_YZK7Tap-kz9MvlJuoAsEC6VpqNl7U0',
+//   authDomain: 'chat-app-official-9f0ee.firebaseapp.com',
+//   projectId: 'chat-app-official-9f0ee',
+//   storageBucket: 'chat-app-official-9f0ee.appspot.com',
+//   messagingSenderId: '234039602872',
+//   appId: '1:234039602872:web:bc3374219f6b08c1de8e4d',
+//   measurementId: 'G-QDG1CZ0LNM',
+// };
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyCL_YZK7Tap-kz9MvlJuoAsEC6VpqNl7U0',
-  authDomain: 'chat-app-official-9f0ee.firebaseapp.com',
-  projectId: 'chat-app-official-9f0ee',
-  storageBucket: 'chat-app-official-9f0ee.appspot.com',
-  messagingSenderId: '234039602872',
-  appId: '1:234039602872:web:bc3374219f6b08c1de8e4d',
-  measurementId: 'G-QDG1CZ0LNM',
+  apiKey: 'AIzaSyBrdKvHKuOLQvDG7SU1isAQZ3TmUiiSywg',
+  authDomain: 'chat-app-official-2.firebaseapp.com',
+  projectId: 'chat-app-official-2',
+  storageBucket: 'chat-app-official-2.appspot.com',
+  messagingSenderId: '401135317678',
+  appId: '1:401135317678:web:0da9d15e5117a6dafd333f',
+  measurementId: 'G-EH71WJRSP2',
 };
 
 const app = initializeApp(firebaseConfig);
