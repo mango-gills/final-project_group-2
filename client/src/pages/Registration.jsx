2eb07005949d9b51023c7b93ef98c5607305d2cd
mary11lane <lanebasa22@gmail.com>
2022-06-20 17:17:21 +0800

    err handling with switch

--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -68,7 +68,26 @@ const Registration = () => {
       // });
       navigate('/conversations');
     } catch (err) {
-      setData({ ...data, error: 'Email is already in use', loading: false });
+      // setData({ ...data, error: 'Email is already in use', loading: false });
+      switch (err.message) {
+        case 'Firebase: Error (auth/email-already-in-use).':
+          setData({ ...data, error: 'Email already in use.', loading: false });
+          break;
+        case 'Firebase: Password should be at least 6 characters (auth/weak-password).':
+          setData({
+            ...data,
+            error: 'Password should be at least 6 characters.',
+            loading: false,
+          });
+          break;
+        default:
+          setData({
+            ...data,
+            error: 'An error occured. Try again.',
+            loading: false,
+          });
+          break;
+      }
     }
   };
 
@@ -93,26 +112,7 @@ const Registration = () => {
       });
       navigate('/conversations');
     } catch (err) {
-      // setData({ ...data, error: 'Email is already in use', loading: false });
-      switch (err.message) {
-        case 'Firebase: Error (auth/email-already-in-use).':
-          setData({ ...data, error: 'Email already in use.', loading: false });
-          break;
-        case 'Firebase: Password should be at least 6 characters (auth/weak-password).':
-          setData({
-            ...data,
-            error: 'Password should be at least 6 characters.',
-            loading: false,
-          });
-          break;
-        default:
-          setData({
-            ...data,
-            error: 'An error occured. Try again.',
-            loading: false,
-          });
-          break;
-      }
+      setData({ ...data, error: 'Email is already in use', loading: false });
     }
 
     // .then((re) => {
